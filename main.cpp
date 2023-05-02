ad4f988cd61eb29bb33c98730fa31725f923b3dc
Ynah Niyel Novilla <ynahnovilla@ucr-secure-42-10-13-218-233.wnet.ucr.edu>
2023-05-01 17:30:44 -0700

    Added main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+    cout << "Welcome to the 8 puzzle solver!" << endl;
+    cout << "Type \"1\" to use a default puzzle, or \"2\" to enter your own puzzle." << endl;
+    int userInput;
+    cin >> userInput;
+
+    if (userInput == 1) {
+        int 
+
+    }
+
+
+    // else {
+
+
+    // }
+}
