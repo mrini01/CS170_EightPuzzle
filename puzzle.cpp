bfaba8f3f525517b5a1294841c4c785ec053c65b
Ynah Niyel Novilla <ynahnovilla@ucr-secure-42-10-13-219-160.wnet.ucr.edu>
2023-05-02 14:25:48 -0700

    Modified puzzle.cpp and puzzle.h

--- puzzle.cpp
+++ puzzle.cpp
@@ -1,5 +1,21 @@
-using namespace std;
 #include <iostream>
 #include "node.h"
 #include "puzzle.h"
 
+using namespace std;
+
+void Puzzle::display_puzzle(const Node* head) {
+    this->root = nullptr;
+}
+
+bool Puzzle::check_if_solved(const Node* head) {
+    return false;
+}
+
+bool Puzzle::check_duplicate(const Node* head) {
+    return false;
+}
+
+void Puzzle::add_puzzle(Node* parent) {
+    this->root = nullptr;
+}
--- puzzle.h
+++ puzzle.h
@@ -1,16 +1,19 @@
-using namespace std;
 #include <iostream>
 #ifndef __PUZZLE_H__
 #define __PUZZLE_H__
 #include "node.h"
 
+using namespace std;
+
 class Puzzle {
     private:
-    Node* root
+    Node* root;
 
     public:
-        void display_puzzle(const Node *head); // display
-        bool check_if_solved(const Node *head); // checks if solved
+        void display_puzzle(const Node* head); // display
+        bool check_if_solved(const Node* head); // checks if solved
+        bool check_duplicate(const Node* parent); // checks a node to see if there is a duplicate puzzle
+        void add_puzzle(Node* parent); // adds a node's puzzle to a list of puzzle to check later
 };
 
 #endif
