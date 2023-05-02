2de044dc92c63dda40318692e7a9832d232d64da
Ynah Niyel Novilla <ynahnovilla@ucr-secure-42-10-13-219-160.wnet.ucr.edu>
2023-05-02 14:16:03 -0700

    Modified node.h and node.cpp

--- node.cpp
+++ node.cpp
@@ -2,20 +2,22 @@ using namespace std;
 #include <iostream>
 #include "Node.h"
 
-Node::Node() {
-    data = "";
-    parent = nullptr;
-    right = nullptr;
-    left = nullptr;
+Node::Node(int data[3][3], int n, int m) {
+    Node* root = new node();
+    for (int i = 0; i < 3; ++i) {
+        for (int j = 0; j < 3; ++j) {
+            root->GameState[i][j] = data[i][j];
+        }
+    }
+    root->right = nullptr;
+    root->left = nullptr;
+    root-> up = nullptr;
+    root->down = nullptr;
+    root->n = n;
+    root->m = m;
+    return root;
 }
 
-Node::Node(string d, Node* p, Node* r, Node* l) {
-    data = d;
-    parent = p;
-    right = r;
-    left = l;
-}
-
-Node::Node(string d) {
-    data = d;
+void Node::expand_node(Node* &parent) {
+    // add in here!
 }
--- node.h
+++ node.h
@@ -7,14 +7,18 @@ using namespace std;
 #define __NODE_H__
 
 class Node {
-    public: 
-        string data;
-        Node* parent;
-        Node* right;
-        Node* left;
+    private:
+        Node* right; // node of parent that is the right move
+        Node* left; // node of parent that is the left move
+        Node* up; // node of parent that is the up move
+        Node* down; // node of parent that is the down move
+        Node* n; // row
+        Node* m; // column
+
+    public:
         Node();
-        Node(string);
-        Node(string, Node*, Node*, Node*);
+        Node(int data[3][3], int n, int m); // creating a node that has null pointers
+        void expand_node(Node* &parent);
 };
 
 #endif;
