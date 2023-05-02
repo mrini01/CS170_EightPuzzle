a21b8b126fbd9b0fc0412677ea1808d535bc0ad7
mrini01 <mrinisha26@gmail.com>
2023-05-01 17:39:09 -0700

    main add

---
+++ eight_puzzle.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+
+using namespace std;
+
+int puzzle[size]
+int userInput;
+
+int main(){
+
+    g(n)_depthValue = 0;			
+	cin >> userInput;
+	if (userInput == 1)
+	{   
+        //default puzzle row 1
+		puzzle[0] = 1;
+		puzzle[1] = 2;
+		puzzle[2] = 3;
+
+        //default puzzle row 2
+		puzzle[3] = 4;
+		puzzle[4] = 0;
+		puzzle[5] = 6;
+
+        //default puzzle row 3
+		puzzle[6] = 7;
+		puzzle[7] = 5;
+		puzzle[8] = 8;
+
+		cout << "Enter your choice of algorithm: " << endl;
+		cout << endl << "1. Uniform Cost Search ";
+		cout << endl << "2. A* with Misplaced Tile heuristic ";
+		cout << endl << "3. A* with Manhattan distance heuristic " << endl;
+		cin >> userInput;
+    }
+
+
+}
